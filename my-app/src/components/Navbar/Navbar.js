b5ed944acdd08e35598cba0da33062b6282cef89
aamirs <aamirs@students.cs.ubc.ca>
2020-06-16 20:57:25 -0700

    Added Bootstrap

--- my-app/src/components/Navbar/Navbar.js
+++ my-app/src/components/Navbar/Navbar.js
@@ -5,7 +5,7 @@ import NavbarItem from "./NavbarItem";
 class Navbar extends React.Component {
   render() {
     return (
-      <div className="navbar" id="navbar">
+      <div className="navbarCustom" id="navbar">
         <NavbarItem />
       </div>
     );
