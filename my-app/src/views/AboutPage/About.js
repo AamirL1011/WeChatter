314623a919a99d1944836a7df7ac09bf663431c2
aamirs <aamirs@students.cs.ubc.ca>
2020-06-16 23:54:44 -0700

    updates

--- my-app/src/views/AboutPage/About.js
+++ my-app/src/views/AboutPage/About.js
@@ -9,8 +9,8 @@ class About extends React.Component {
         <div id="about" className="container-div">
           <h1>About the Developer</h1>
           <div className="msgFeed">
-            <p>Hi! I am a 3rd year computer science student at UBC!</p>
-            <p>This is a web app where you can post chatter!</p>
+            <p>Hi! I am a 3rd year computer science student.</p>
+            <p>This is a React web app where you can post the latest chatter!</p>
           </div>
         </div>
       </section>
